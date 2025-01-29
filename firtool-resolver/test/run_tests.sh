d93913095fa4f110ad37965a8aaac6b23dc2f65c
Jack Koenig <koenig@sifive.com>
2025-01-28 17:46:06 -0800

    DEBUG add -v to FileCheck invocations

--- firtool-resolver/test/run_tests.sh
+++ firtool-resolver/test/run_tests.sh
@@ -14,20 +14,20 @@ export LLVM_FIRTOOL_VERSION=$(./mill show llvm-firtool.publishVersion | xargs)
 # If there's a -SNAPSHOT in LLVM_FIRTOOL_VERSION, strip it
 export FIRTOOL_VERSION=${LLVM_FIRTOOL_VERSION%-SNAPSHOT}
 
-$THIS_DIR/negative.sh 2>&1 | FileCheck $THIS_DIR/negative.sh
+$THIS_DIR/negative.sh 2>&1 | FileCheck -v $THIS_DIR/negative.sh
 
 # TODO remove this line, use maven central published versions
 ./mill llvm-firtool.publishLocal
 
 ./mill firtool-resolver[$SCALA_VERSION].publishLocal
 
-$THIS_DIR/on_classpath.sh 2>&1 | FileCheck -DLLVM_FIRTOOL_VERSION="$LLVM_FIRTOOL_VERSION" -DFIRTOOL_VERSION="$FIRTOOL_VERSION" $THIS_DIR/on_classpath.sh
+$THIS_DIR/on_classpath.sh 2>&1 | FileCheck -v -DLLVM_FIRTOOL_VERSION="$LLVM_FIRTOOL_VERSION" -DFIRTOOL_VERSION="$FIRTOOL_VERSION" $THIS_DIR/on_classpath.sh
 
-$THIS_DIR/installed.sh 2>&1 | FileCheck -DLLVM_FIRTOOL_VERSION="$LLVM_FIRTOOL_VERSION" -DFIRTOOL_VERSION="$FIRTOOL_VERSION" $THIS_DIR/installed.sh
+$THIS_DIR/installed.sh 2>&1 | FileCheck -v -DLLVM_FIRTOOL_VERSION="$LLVM_FIRTOOL_VERSION" -DFIRTOOL_VERSION="$FIRTOOL_VERSION" $THIS_DIR/installed.sh
 
-$THIS_DIR/fetched.sh 2>&1 | FileCheck -DLLVM_FIRTOOL_VERSION="$LLVM_FIRTOOL_VERSION" -DFIRTOOL_VERSION="$FIRTOOL_VERSION" $THIS_DIR/fetched.sh
+$THIS_DIR/fetched.sh 2>&1 | FileCheck -v -DLLVM_FIRTOOL_VERSION="$LLVM_FIRTOOL_VERSION" -DFIRTOOL_VERSION="$FIRTOOL_VERSION" $THIS_DIR/fetched.sh
 
-$THIS_DIR/firtool_path.sh 2>&1 | FileCheck -DLLVM_FIRTOOL_VERSION="$LLVM_FIRTOOL_VERSION" -DFIRTOOL_VERSION="$FIRTOOL_VERSION" $THIS_DIR/firtool_path.sh
+$THIS_DIR/firtool_path.sh 2>&1 | FileCheck -v -DLLVM_FIRTOOL_VERSION="$LLVM_FIRTOOL_VERSION" -DFIRTOOL_VERSION="$FIRTOOL_VERSION" $THIS_DIR/firtool_path.sh
 
 cache="$PWD/firtool_cache"
 if [[ "$OS" == "windows" ]]; then
@@ -37,4 +37,4 @@ else
   filecheck_cache=$cache
 fi
 rm -rf $cache
-CHISEL_FIRTOOL_CACHE=$cache $THIS_DIR/firtool_cache.sh 2>&1 | FileCheck -DLLVM_FIRTOOL_VERSION="$LLVM_FIRTOOL_VERSION" -DFIRTOOL_VERSION="$FIRTOOL_VERSION" -DCHISEL_FIRTOOL_CACHE="$filecheck_cache" $THIS_DIR/firtool_cache.sh
+CHISEL_FIRTOOL_CACHE=$cache $THIS_DIR/firtool_cache.sh 2>&1 | FileCheck -v -DLLVM_FIRTOOL_VERSION="$LLVM_FIRTOOL_VERSION" -DFIRTOOL_VERSION="$FIRTOOL_VERSION" -DCHISEL_FIRTOOL_CACHE="$filecheck_cache" $THIS_DIR/firtool_cache.sh
