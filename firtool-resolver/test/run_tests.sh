4a3a686fcaf63daf909cc7cac25a4d82b1c8912b
Jack Koenig <koenig@sifive.com>
2026-04-03 13:41:07 -0700

    Rename LLMV_FIRTOOL_VERSION to FIRTOOL_VERSION since thats what it is

--- firtool-resolver/test/run_tests.sh
+++ firtool-resolver/test/run_tests.sh
@@ -11,8 +11,6 @@ export COURSIER_CACHE="$PWD/coursier_cache"
 rm -rf $COURSIER_CACHE
 export FIRTOOL_RESOLVER_VERSION=$(./mill show firtool-resolver[$SCALA_VERSION].publishVersion | xargs)
 export LLVM_FIRTOOL_VERSION=$(./mill show llvm-firtool.publishVersion | xargs)
-# If there's a -SNAPSHOT in LLVM_FIRTOOL_VERSION, strip it
-export FIRTOOL_VERSION=${LLVM_FIRTOOL_VERSION%-SNAPSHOT}
 
 $THIS_DIR/negative.sh 2>&1 | FileCheck $THIS_DIR/negative.sh
 
